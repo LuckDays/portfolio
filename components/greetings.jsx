f1c380de6c25f4098779fa683f1c3474fb5130ec
Lucas S D Frota <93730871+LuckDays@users.noreply.github.com>
2022-08-18 02:29:20 -0300

    fixing image

--- components/greetings.jsx
+++ components/greetings.jsx
@@ -1,11 +1,9 @@
-import Image from "next/Image";
-
 function Greetings() {
   return (
     <div className='bg-blue-900 flex flex-row items-center justify-center w-full bg-opacity-10'>
       <section
         id='/'
-        className='grid grid-cols-1 md:grid-cols-2 container items-center justify-center md:h-[240px] lg:h-[320px] xl:h-[400px] 2xl:h-[480px] '
+        className='grid grid-cols-1 grid-rows-2 md:grid-rows-1 md:grid-cols-2 container items-center justify-center md:h-[240px] lg:h-[320px] xl:h-[400px] 2xl:h-[480px] '
       >
         <div className='flex flex-col justify-center px-6'>
           <h2 className='text-4xl md:text-5xl'>Lucas de Sousa Dias Frota</h2>
@@ -19,10 +17,14 @@ function Greetings() {
           </em>
         </div>
         <div className='flex items-center justify-center relative'>
-          <div className='rounded-full h-36 w-36 md:h-44 md:w-44 opacity-90 blur-xl bg-colorPrimary absolute -translate-x-16 animate-pulse' />
+          <div className='rounded-full h-32 w-32 md:h-44 md:w-44 opacity-90 blur-xl bg-colorPrimary absolute -translate-x-16 animate-pulse' />
           <div className='rounded-full h-24 w-24 md:h-36 md:w-36 opacity-90 blur-xl bg-colorSecondary translate-x-7 absolute animate-pulse' />
-          <div>
-            <Image src='/banner.png' alt='banner' width='1200' height='750' />
+          <div className='absolute'>
+            <img
+              src='/banner.png'
+              alt='banner1'
+              className=' h-[150px] md:h-[240px] lg:h-[320px] xl:h-[400px] 2xl:h-[480px] md:w-[1200px]'
+            />
           </div>
         </div>
       </section>
